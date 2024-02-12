e79d2361258f631b83c615dd63dae4e08e9b8063
kavinshah <38910831+kavinshah@users.noreply.github.com>
2024-02-12 14:07:25 +0530

    adding home page

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+import React, {useState} from 'react';
+
+function Home(){
+	return (
+		<div className='center'>
+		{/*<a id='single' className='btn btn-primary' href='tictactoe?mode=singleplayer'>Play with computer</a>*/}
+			<a id='twoplayer' className='btn btn-primary' href='tictactoe?mode=multiplayer'>Play with another human</a>
+		</div>
+	);
+}
+
+export default Home;
